94dedd2c64f03a6477cc91064f1b46a1fba19910
Ujjawal <ujjawalgupta23@gmail.com>
2023-04-08 18:40:10 +0530

    Add: Shifted MENU
    Shifted MENU and resources to new file

--- coffee machine/Coffee Machine.py
+++ coffee machine/Coffee Machine.py
@@ -1,34 +1,4 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 20.0,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 40.0,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 60.0,
-    }
-}
-
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+from coffee_menu_and_resource import MENU, resources
 
 
 def check_resources(items):
